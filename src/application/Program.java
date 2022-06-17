4e42fa0a1d1b61a54f106ed5ba1c41a77af36932
Weverson Muciarone <weverson.muciarone@gmail.com>
2022-06-17 13:45:17 +0100

    Reusing Instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,8 @@ public class Program {
 
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("=== Teste findById===");
 		Seller seller = sellerDao.findById(3);
 		
 		
