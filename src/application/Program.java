0e759b44357056f0537611c5b4801b45d51f1a00
Weverson Muciarone <weverson.muciarone@gmail.com>
2022-06-17 14:52:53 +0100

    Inserted new Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -31,7 +32,10 @@ public class Program {
 			System.out.println(obj);
 		}
 
-		
+		System.out.println("\n=== Teste4 INSERT===");
+		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.0,department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id = " + newSeller.getId());
 
 	}
 
