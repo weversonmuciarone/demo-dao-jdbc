4dda21a26a006c9c27a11f712d5836ca39be1d46
Weverson Muciarone <weverson.muciarone@gmail.com>
2022-06-17 15:05:12 +0100

    Seller Updated

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,12 @@ public class Program {
 		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.0,department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
+		
+		System.out.println("\n=== Teste5 UPDATE===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 
 	}
 
